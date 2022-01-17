5b9602696be03757e84c7837e428a550eb78e7a7
olgatenison <88651744+olgatenison@users.noreply.github.com>
2022-01-17 02:02:33 +0200

    update task 5

--- js/task-05.js
+++ js/task-05.js
@@ -6,9 +6,15 @@ const nameOutput = document.querySelector('#name-output > span');
 console.log(nameOutput);
 
 
+
 inputForm.addEventListener('input', onInputChange)
 
 function onInputChange(event){
 console.log(event.currentTarget.value)
 nameLabel.textContent = event.currentTarget.value
+    
+    if(event.currentTarget.value === "")
+{nameLabel.textContent = "Anonymous"} 
+else {nameLabel.textContent = event.currentTarget.value
+}
 }
