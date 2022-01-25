eb2d6cb1b588365fe32c43e20b0ae46e40616f01
olgatenison <88651744+olgatenison@users.noreply.github.com>
2022-01-25 13:07:51 +0200

    add task 6

---
+++ js/task-06.js
@@ -0,0 +1,15 @@
+const inputEl = document.querySelector('#validation-input');
+inputEl.addEventListener('blur', onInputHandler);
+
+function onInputHandler(event){
+   
+    if (event.currentTarget.value.length === Number(inputEl.dataset.length)){
+        inputEl.classList.remove('invalid')
+        inputEl.classList.add('valid') 
+    } else {
+        inputEl.classList.remove('valid')
+        inputEl.classList.add('invalid')
+    }
+}
+
+
