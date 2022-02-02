cdc01b805d2b83016db01a73b6c788d39ec40935
olgatenison <88651744+olgatenison@users.noreply.github.com>
2022-02-02 23:16:27 +0200

    add tast 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,18 @@
+
+const  colorRefs = {
+  body: document.querySelector('body'),
+  descr: document.querySelector('.color'),
+  btn: document.querySelector('.change-color'),
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+colorRefs.btn.addEventListener ('click', changeBgColor);
+
+function changeBgColor (event){
+
+  colorRefs.body.style.backgroundColor = getRandomHexColor();
+  colorRefs.descr.textContent = `${getRandomHexColor()}`;
+ 
 }
