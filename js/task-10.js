9273ff8fed4b5b1e077dc27100d32f18a83618ec
olgatenison <88651744+olgatenison@users.noreply.github.com>
2022-02-05 15:29:55 +0200

    update task10

--- js/task-10.js
+++ js/task-10.js
@@ -9,19 +9,33 @@ const refs = {
   createBtn: document.querySelector('[data-create]'),
   destroyBtn: document.querySelector('[data-destroy]'),
   boxes: document.querySelector("#boxes"),
+  
 }
 
 refs.createBtn.addEventListener("click", createBoxes);
-//refs.destroyBtn.addEventListener("click", destroyBoxes);
+refs.destroyBtn.addEventListener("click", destroyBoxes);
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 };
 
 function createBoxes(amount){
-  //amount = refs.input.value
-  //console.log(amount)
+  amount = refs.input.value
+  let size = 30;
+  for (let i = 0; i < amount; i++) {
+   size += 10; 
+
+  const newBoxes = document.createElement("div"); 
+  newBoxes.style.width = `${size}px`;  
+  newBoxes.style.height = `${size}px`;   
+  newBoxes.style.backgroundColor = getRandomHexColor();
+  newBoxes.style.margin = '10px';  
+
+  boxes.append(newBoxes);  
+ 
 }
-//function destroyBoxes(){
-//  refs.boxes.innerHTML = '';
-//}
+}
+function destroyBoxes(){
+//refs.boxes.remove(newBoxes);
+refs.boxes.innerHTML = '';
+}
